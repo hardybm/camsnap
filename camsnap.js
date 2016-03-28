a35dd042187747be61298c1ac386ccb37ae18c76
Ben Hardy <ben@hardy.cx>
2016-03-28 17:40:17 +1100

    change to util.log

--- camsnap.js
+++ camsnap.js
@@ -8,15 +8,16 @@ var MjpegCamera = require('mjpeg-camera');
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var util = require('util');
 
 app.listen(port, function () {
-	console.log('Server running on port ' + port);
+	util.log( 'Server running on port ' + port);
 });
 
 var camera = new MjpegCamera( { url: camPath } );
 
 app.get( '/snap.jpg', function (req, res) {
-	console.log('GET \'/snap.jpg\'');
+	util.log('GET \'/snap.jpg\'');
 	camera.getScreenshot( function (err, frame ) {	
 		//res.writeHead(200, {'Content-Type': 'text/plain' });
 		res.send( frame )
